885d2ac8291be7bba247ac65e1761f9abc2089f9
afista pratama <pratama.otori.12@gmail.com>
2021-01-07 23:15:49 +0700

    Adding router 3rd API

--- server/routes/activityRouter.js
+++ server/routes/activityRouter.js
@@ -6,10 +6,17 @@ const Controller = require('../controller/activityController')
 router.use(authenticate)
 router.get('/', Controller.getActivity)
 router.post('/', Controller.createActivity)
+router.get('/music', Controller.musicList)
+router.get('/news', Controller.cnnNews)
+router.get('/covid', Controller.covidUpdate)
+router.get('/movie', Controller.movieList)
+
 router.get('/:id', authorize, Controller.getActivityById)
 router.put('/:id', authorize, Controller.editActivityById)
 router.patch('/:id', authorize, Controller.changeStatus)
 router.delete('/:id', authorize, Controller.deleteActivity)
+
+
 
 
 module.exports = router
