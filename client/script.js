510468f49c155a48254ee1ea3e2fb90174a63ac0
Fauzi Ramadan <fauzidanrama@gmail.com>
2021-01-07 21:08:29 +0700

    add-login-register-form-with-jquery

---
+++ client/script.js
@@ -0,0 +1,10 @@
+$(".register").hide()
+$(".main-content").hide()
+$("#toRegister").click( () => {
+    $(".register").show()
+    $(".login").hide()
+} )
+$("#toLogin").click( () => {
+    $(".register").hide()
+    $(".login").show()
+} )
